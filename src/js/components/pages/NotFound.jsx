a671ea2edd2874647047baca621d79f033504625
Thibaud B <thibaud.tibo@gmail.com>
2015-02-05 23:58:11 +0100

    style NotFound page

--- src/js/components/pages/NotFound.jsx
+++ src/js/components/pages/NotFound.jsx
@@ -4,14 +4,16 @@ var React = require('react');
 
 var NotFound = React.createClass({
 
-  render: function() {
-    return (
-      <div>
-        <h1>404</h1>
-        <p>Page not found.</p>
-      </div>
-    );
-  }
+	render: function() {
+		return (
+			<div className="error error-404">
+				<div className="error-info">
+					<h1>404</h1>
+					<p>Page not found.</p>
+				</div>
+			</div>
+		);
+	}
 
 });
 
