bf5c817f44d9212cb2fa844f5077640494a3477b
Thibaud B <thibaud.tibo@gmail.com>
2015-02-08 18:57:31 +0100

    style Player page

--- src/js/components/pages/Player.jsx
+++ src/js/components/pages/Player.jsx
@@ -6,17 +6,21 @@ var AppStore       = require('../../stores/AppStore');
 var StoreWatchMixin = require('../../mixins/StoreWatchMixin');
 var Loading 			 = require('../partials/Loading.jsx');
 var NotFound       = require('../pages/NotFound.jsx');
-var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+var Navbar         = require('../partials/Navbar.jsx');
+var Header         = require('../partials/Header.jsx');
+var Footer         = require('../partials/Footer.jsx');
+// var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 var DocumentTitle	 = require('react-document-title');
 var Router         = require('react-router');
 var RouteHandler   = Router.RouteHandler;
 var Link           = Router.Link;
+var Navigation     = Router.Navigation;
 
 
 var Player = React.createClass({
 
-	mixins: [StoreWatchMixin],
+	mixins: [StoreWatchMixin, Navigation],
 
 	getData: function(props) {
 		var props = props.id || props;
@@ -34,11 +38,18 @@ var Player = React.createClass({
 			var shots = this.state.data.shots;
 			var shotsNode = shots.map(function(shot, i) {
 				return (
-					<li key={i}>
-						<Link to="shot" params={{id: shot.id}}>
-							{shot.title} <img width="400" height="300" src={shot.image_url}/>
-						</Link>
-					</li>
+					<div key={i} className="col-3">
+						<div className="shot animated zoomIn">
+							<Link className="shot__link" to="shot" params={{id: shot.id}}>
+								<img width="400" height="300" src={shot.image_url}/>
+							</Link>
+							<p>
+								<span><i className="fa fa-heart"></i></span>{shot.likes_count}
+								<span><i className="fa fa-eye"></i></span>{shot.views_count}
+								<span><i className="fa fa-comment"></i></span>{shot.comments_count}
+							</p>
+						</div>
+					</div>
 				);
 			});
 			return shotsNode;
@@ -46,14 +57,19 @@ var Player = React.createClass({
 	},
 
 	renderPlayer: function() {
-		// var shots = this.state.data.shots;
 		if (typeof(this.state.data.shots) !== 'undefined') {
 			var player = this.state.data.shots[0].player;
 			return (
-				<Link to="player" params={{id: player.username}}>
-					<h2>{player.name}</h2>
-					<img className="img-avatar" width="160" height="160" src={player.avatar_url}/>
-				</Link>
+				<div className="player__card page">
+					<div className="player__banner animated moveDown"></div>
+					<div className="player__info">
+						<figure className="img-avatar animated zoomIn">
+							<img width="160" height="160" src={player.avatar_url}/>
+						</figure>
+						<h2 className="animated fadeInLeft">{player.name}</h2>
+						<p className="animated fadeInRight"><i className="fa fa-map-marker"></i> {player.location}</p>
+					</div>
+				</div>
 			);
 		}
 	},
@@ -64,18 +80,24 @@ var Player = React.createClass({
 
 		var loading = data.length === 0 ? <Loading /> : '';
 
-		if (id == 'error') {
-			return (
-				<NotFound/>
-			);
-		}
+		if (id == 'error') 
+			return <NotFound/>
 
 		return (
-			<div>
+			<div className="player-page">
+				<Navbar />
+				<Header title="player" />
 				{loading}
-				<div>{this.renderPlayer()}</div>
-				<hr/>
-				<div>{this.renderShots()}</div>
+				<div className="player">
+					{this.renderPlayer()}
+				</div>
+				<div className="container">
+					<hr/>
+				</div>
+				<div className="container">
+					{this.renderShots()}
+				</div>
+				<Footer />
 			</div>
 		);
 	},
