c81350d2d0262ec54dc3be7556627e045557edbd
Thibaud B <thibaud.tibo@gmail.com>
2015-02-08 18:54:58 +0100

    style notFound

--- src/js/components/pages/NotFound.jsx
+++ src/js/components/pages/NotFound.jsx
@@ -1,13 +1,17 @@
 'use strict';
 
 var React = require('react');
+var Navbar         = require('../partials/Navbar.jsx');
 var Footer         = require('../partials/Footer.jsx');
 
 var NotFound = React.createClass({
 
 	render: function() {
 		return (
-			<div className="error-page error-404">
+			<div className="error-page page error-404">
+
+				<Navbar />
+
 				<div className="error__inner">
 					<h1>404</h1>
 					<p>Page not found.</p>
